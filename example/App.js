35339f26a64696da9e44b55e9524555ac9998e32
HackingGate <i@hackinggate.com>
2021-01-18 23:10:33 +0900

    Update example

--- example/App.js
+++ example/App.js
@@ -1,50 +1,55 @@
 import React, {useState} from 'react';
-import {Button, StyleSheet, Text, TouchableOpacity, View, SafeAreaView} from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
 import TooltipWrapper from 'react-native-hg-tooltip';
 
 function App() {
-  const [visible, setVisible] = useState(false);
-
-  function toggleVisible(value) {
-    setVisible(value);
-  }
+  const [tooltip1, setTooltip1] = useState(false);
+  const [tooltip2, setTooltip2] = useState(false);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Button
-        title={'Toggle Visible'}
-        onPress={() => toggleVisible(!visible)}
-      />
-      <View style={{flex: 1, justifyContent: 'center'}}>
-        <TooltipWrapper
-          isVisible={visible}
-          position={'bottom'}
-          modalStyle={{backgroundColor: 'rgba(0, 0, 0, 0.3)'}}
-          tooltipStyle={{
-            backgroundColor: 'cyan',
-            height: 40,
-            width: 220,
-            borderRadius: 10,
-          }}
-          arrowBase={24}
-          arrowHeight={16}
-          arrowColor={'cyan'}
-          distanceToTarget={5}
-          tooltipComponent={
-            <TouchableOpacity
-              style={{
-                width: '100%',
-                height: '100%',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-              onPress={() => toggleVisible(false)}>
-              <Text>This is a tooltip.</Text>
-            </TouchableOpacity>
-          }>
-          <Text>Target</Text>
-        </TooltipWrapper>
-      </View>
+      <TooltipWrapper
+        isVisible={tooltip1}
+        position={'top'}
+        modalStyle={{backgroundColor: 'rgba(0, 0, 0, 0.3)'}}
+        tooltipStyle={styles.tooltipStyle}
+        arrowBase={24}
+        arrowHeight={16}
+        arrowColor={'cyan'}
+        distanceToTarget={5}
+        tooltipComponent={
+          <TouchableOpacity
+            style={styles.tooltipTouchable}
+            onPress={() => setTooltip1(false)}>
+            <Text>This is tooltip1</Text>
+          </TouchableOpacity>
+        }>
+        <Button title={'Show tooltip1'} onPress={() => setTooltip1(true)} />
+      </TooltipWrapper>
+      <TooltipWrapper
+        isVisible={tooltip2}
+        position={'bottom'}
+        modalStyle={{backgroundColor: 'rgba(0, 0, 0, 0.3)'}}
+        tooltipStyle={styles.tooltipStyle}
+        arrowBase={24}
+        arrowHeight={16}
+        arrowColor={'cyan'}
+        distanceToTarget={5}
+        tooltipComponent={
+          <TouchableOpacity
+            style={styles.tooltipTouchable}
+            onPress={() => setTooltip2(false)}>
+            <Text>This is tooltip2</Text>
+          </TouchableOpacity>
+        }>
+        <Button title={'Show tooltip2'} onPress={() => setTooltip2(true)} />
+      </TooltipWrapper>
     </SafeAreaView>
   );
 }
@@ -53,6 +58,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tooltipStyle: {
+    backgroundColor: 'cyan',
+    height: 40,
+    width: 220,
+    borderRadius: 10,
+  },
+  tooltipTouchable: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
     alignItems: 'center',
   },
 });
